7fdc36fcf4dbc49a74ae900d01b8bf3bdb671bed
Eemeli Saari <saari.eemeli@gmail.com>
2018-10-03 10:48:34 +0300

    Finished Task 3

--- Basics/exercise9/task3.m
+++ Basics/exercise9/task3.m
@@ -1,21 +1,29 @@
 % Task 3
 
+% Low-pass filter truncated between -20 <= n <= 20
+
+%% Define the variables used in the task
+
 fc = 0.3;
 fs = 0.5;
 
-%%
+%% Estimate the first oscillatory peak in the stopband
+
 n = -20:fs:20;
+freqz((sinc(n * fc)*2*fc))
 
-plot(n, abs(sinc(n * fc)*2*fc))
+% Answer: 0.18
 
-%%
+%% Repeat with truncation -30 <= n <= 30.
 
 n = -30:fs:30;
+freqz((sinc(n * fc)*2*fc))
 
-plot(n, abs(sinc(n * fc)*2*fc))
+% Answer: Still the same - not as big
 
-%%
+%% What about with even bigger truncation?
 
 n = -300:fs:300;
+freqz((sinc(n * fc)*2*fc))
 
-plot(n, abs(sinc(n * fc)*2*fc))
+% Answer: Remains the same
