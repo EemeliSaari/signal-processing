c137c16405f2633ed62265a7d4a21118f69a731d
Eemeli Saari <saari.eemeli@gmail.com>
2018-10-03 10:48:58 +0300

    Finished Task4

--- Basics/exercise9/task4.m
+++ Basics/exercise9/task4.m
@@ -7,7 +7,8 @@
 load('number.mat');
 
 specgram(secret)
-%%
+
+%% Assign variables according to the task
 
 a = 0.0;
 b = 0.33;
@@ -15,18 +16,23 @@ c = 0.36;
 d = 1.0;
 
 N = 98;
-%%
 
-f=firpm(N, [a,b,c,d], [0, 0, 1, 1]);
+%% Define the Parks-McClellan filter
 
-%%
+f = firpm(N, [a, b, c, d], [0, 0, 1, 1]);
+
+%% Plot the Amplitude response
 
 freqz(f)
 
-%%
+%% Filter the signal
 
-y = conv(secret, f);
+y = conv(secret, f); % Here I used the convolution instead of filter
 
-%%
+%% Listen to the sound
 
 soundsc(y)
+
+%% Plot the resulted specgram
+
+specgram(y)
